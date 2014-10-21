9c31aa2c1c62383293483acbd7df4e6c479f03dc
Tam T Dang <teedang19@gmail.com>
2014-10-21 14:16:07 -0500

    default changes after running rails generate devise:install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  
+  # TODO: "host" should be set to the actual host of your application.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false

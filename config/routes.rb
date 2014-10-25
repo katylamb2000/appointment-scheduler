aae0f3b2578caf13a3bd06449010323641a1611d
Tam T Dang <teedang19@gmail.com>
2014-10-25 08:35:04 -0500

    specify empty path for devise for users, to remove users prefix on urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, path: ''
 
   # Example resource route with options:
   #   resources :products do

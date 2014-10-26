9440ed1d059a5e1d50e34e6ecda739445adc5441
Tam T Dang <teedang19@gmail.com>
2014-10-25 20:18:42 -0500

    validations are skipped for admins and instructors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :async
 
-  validates_presence_of :first_name, :city, :country, :age
+  validates_presence_of :first_name, unless: Proc.new { |u| u.admin? }
+  validates_presence_of :city, :country, :age, unless: Proc.new { |u| u.admin? || u.instructor? }
+
   validates :gender, inclusion: { in: ["male", "female"] }, allow_nil: true
 end

98dc863f4f3556dc0cc14059464bed54ca4cd033
Tam T Dang <teedang19@gmail.com>
2014-10-25 20:14:12 -0500

    validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :async
+
+  validates_presence_of :first_name, :city, :country, :age
+  validates :gender, inclusion: { in: ["male", "female"] }, allow_nil: true
 end

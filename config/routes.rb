d820dae1e380bbd4303167014c6e565203f985f5
Tam T Dang <teedang19@gmail.com>
2014-10-25 19:49:46 -0500

    mount sidekiq GUI at /sidekiq if user is admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'static_pages#home'
+  
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   devise_for :users, path: ''
-
   # Example resource route with options:
   #   resources :products do
   #     member do

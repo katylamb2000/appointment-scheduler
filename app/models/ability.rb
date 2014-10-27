e5255b46a557f0d2701e70943a058d3bc23512b4
Tam T Dang <teedang19@gmail.com>
2014-10-26 19:48:51 -0500

    empty availability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,15 @@ class Ability
   def initialize(user)
     user ||= User.new
     if user.admin?
-        can :access, :rails_admin
-        can :dashboard
-        can :manage, :all
+      can :access, :rails_admin
+      can :dashboard
+      can :manage, :all
+    elsif user.instructor?
+      can :access, :rails_admin
+      can :dashboard
+      can :manage, Availability, :instructor_id => user.id
     else
-        can :manage, User, :id => user.id
+      can :manage, User, :id => user.id
     end
   end
 end

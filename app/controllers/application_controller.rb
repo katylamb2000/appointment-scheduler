4630df779fe867fb70215ecfb08f73e7cf625acf
Tam T Dang <teedang19@gmail.com>
2014-11-05 19:50:08 -0600

    rescue from CanCan access denied with unauthorized alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = exception.message
+    redirect_to(request.referrer || main_app.root_path)
+  end
+
   def after_sign_in_path_for(resource)
     (resource.admin? || resource.instructor?) ? rails_admin_path : root_path
   end

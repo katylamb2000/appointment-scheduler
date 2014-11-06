c81aac5a0c5e86c86d4efd4d9c0b4fee4d5f712c
Tam T Dang <teedang19@gmail.com>
2014-11-05 21:55:10 -0600

    basic appointment status validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,4 @@
 class Appointment < ActiveRecord::Base
   validates_presence_of :user_id, :instructor_id, :appointment_category_id, :start_time, :status
+  validates :status, inclusion: { in: ["Future", "Past - Occurred", "Cancelled by Student", "Cancelled by Instructor", "Rescheduled by Student", "Rescheduled by Instructor", "No Show"] }
 end

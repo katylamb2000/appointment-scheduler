724ad2a89a4053d2a028a47102cdfc07dfdef608
Tam T Dang <teedang19@gmail.com>
2014-11-05 21:32:02 -0600

    AppointmentCategory uniqueness validation on lesson minutes

--- app/models/appointment_category.rb
+++ app/models/appointment_category.rb
@@ -1,5 +1,6 @@
 class AppointmentCategory < ActiveRecord::Base
   validates_presence_of :lesson_minutes, :buffer_minutes, :price_in_cents
+  validates_uniqueness_of :lesson_minutes
   validates :lesson_minutes, :buffer_minutes, :price_in_cents, numericality: { greater_than: 0 }
 
   def total_duration

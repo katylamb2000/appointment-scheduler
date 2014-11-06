c744a563a0158c2e36f6385bfd778ab05c0eb89b
Tam T Dang <teedang19@gmail.com>
2014-11-05 21:30:37 -0600

    appointment_category#total_duration method, returns lesson_minutes+buffer_minutes

--- app/models/appointment_category.rb
+++ app/models/appointment_category.rb
@@ -1,4 +1,8 @@
 class AppointmentCategory < ActiveRecord::Base
   validates_presence_of :lesson_minutes, :buffer_minutes, :price_in_cents
   validates :lesson_minutes, :buffer_minutes, :price_in_cents, numericality: { greater_than: 0 }
+
+  def total_duration
+    lesson_minutes + buffer_minutes
+  end
 end

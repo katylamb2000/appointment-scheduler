5c6b9328f181ef8e8cbce482ac8a976af5d14911
Tam T Dang <teedang19@gmail.com>
2014-11-05 21:41:35 -0600

    rails admin base config for appointment category

--- app/models/appointment_category.rb
+++ app/models/appointment_category.rb
@@ -10,4 +10,48 @@ class AppointmentCategory < ActiveRecord::Base
   def price_in_dollars
     price_in_cents / 100
   end
+
+  rails_admin do
+    list do
+      field :id
+      field :lesson_minutes
+      field :buffer_minutes
+      field :price_in_dollars do
+        label do
+          "Price"
+        end
+        pretty_value do
+          "$#{value}0" # TODO fix this hack
+        end
+      end
+    end
+
+    show do
+      field :id
+      field :price_in_dollars do
+        label do
+          "Price"
+        end
+        pretty_value do
+          "$#{value}0" # TODO fix this hack
+        end
+      end
+      field :lesson_minutes
+      field :buffer_minutes
+      field :total_duration
+    end
+
+    edit do
+      field :lesson_minutes
+      field :buffer_minutes
+      field :price_in_cents do
+        label do
+          "Price"
+        end
+        help do
+          "Required. MUST BE IN CENTS"
+        end
+      end
+    end
+  end
 end

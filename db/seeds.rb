8aa48880898c7ffc3651c054a0c26d8ab714eba6
Tam T Dang <teedang19@gmail.com>
2014-11-05 21:34:27 -0600

    seedfile seeds two basic appt types: 30 and 60 minutes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,9 @@ User.create(email: "admin@sbguitar.com", password: "password", admin: true, firs
 
 shawn = User.find_by(email: "shawn@sbguitar.com")
 User.create(email: "shawn@sbguitar.com", password: "password", instructor: true, first_name: "Shawn", last_name: "Baldissero") unless shawn
+
+thirty_minute_appt = AppointmentCategory.find_by_lesson_minutes(30)
+AppointmentCategory.create(lesson_minutes: 30, buffer_minutes: 15, price_in_cents: 5000) unless thirty_minute_appt
+
+one_hour_appt = AppointmentCategory.find_by_lesson_minutes(60)
+AppointmentCategory.create(lesson_minutes: 60, buffer_minutes: 15, price_in_cents: 9000) unless one_hour_appt

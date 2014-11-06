cdfdcb73ce8d9365fc32dbb83efa11869fdb92f7
Tam T Dang <teedang19@gmail.com>
2014-11-05 21:29:52 -0600

    appointment category validations

--- app/models/appointment_category.rb
+++ app/models/appointment_category.rb
@@ -1,2 +1,4 @@
 class AppointmentCategory < ActiveRecord::Base
+  validates_presence_of :lesson_minutes, :buffer_minutes, :price_in_cents
+  validates :lesson_minutes, :buffer_minutes, :price_in_cents, numericality: { greater_than: 0 }
 end

1a91981986d8f1117a2f9808409cfed0a38e761d
Tam T Dang <teedang19@gmail.com>
2014-11-16 09:18:25 -0500

    Availability validations of 1 hour duration and end time is after start time

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,8 +2,14 @@ class Availability < ActiveRecord::Base
   belongs_to :instructor, class_name: "User"
 
   validates_presence_of :instructor, :start_time, :end_time
-  # TODO validate end time is after start time ?
-  # TODO validate hours are 00 or 30 ?
-  # TODO validate start time and end time are on same day?
+  validate :end_time_must_be_after_start_time, :duration_must_be_at_least_one_hour # TODO validate hours are 00 or 30 ?
+
+  def end_time_must_be_after_start_time
+    errors.add(:end_time, "must be after start time.") unless end_time > start_time
+  end
+
+  def duration_must_be_at_least_one_hour
+    errors.add(:base, "Duration must be at least one hour.") unless (end_time >= start_time + 1.hour)
+  end
 
 end

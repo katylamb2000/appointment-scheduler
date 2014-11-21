c9148b280ad16a2913f271437d46089d3ef8e831
Tam T Dang <teedang19@gmail.com>
2014-11-21 07:24:16 -0600

    list view for rails_admin on guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,12 @@
 class Guest < User
   default_scope { where(guest: true) }
+
+  rails_admin do
+    list do
+      field :email
+      field :city
+      field :country
+      field :age
+    end
+  end
 end

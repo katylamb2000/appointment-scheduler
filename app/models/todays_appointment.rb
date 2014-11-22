d302032df292778057caeb0ab0509ccda123f6d8
Tam T Dang <teedang19@gmail.com>
2014-11-22 09:16:05 -0600

    Todays Appointment rails admin label method

--- app/models/todays_appointment.rb
+++ app/models/todays_appointment.rb
@@ -1,3 +1,9 @@
 class TodaysAppointment < Appointment
   default_scope { where('start_time > ?', Date.today.beginning_of_day).where('end_time < ?', Date.today.end_of_day) }
+
+  rails_admin do
+    label do
+      "Today's Appointment"
+    end
+  end
 end

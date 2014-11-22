6ef4b4f0f9dda52e476ede4f9edc160ff8c19cf3
Tam T Dang <teedang19@gmail.com>
2014-11-21 21:23:10 -0600

    migration to add end_time to appointments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117223352) do
+ActiveRecord::Schema.define(version: 20141122032052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141117223352) do
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "end_time"
   end
 
   create_table "availabilities", force: true do |t|

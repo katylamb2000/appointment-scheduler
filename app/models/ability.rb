92cac587c17ab6bedfe3b82dd90d65770746f792
Tam T Dang <teedang19@gmail.com>
2014-11-21 22:38:25 -0600

    instructor can read User where self, read and update Appointments where they are the instructor, and comments about future features for instructors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,9 +8,17 @@ class Ability
       can :dashboard
       can :manage, :all
     elsif user.instructor?
+      # update own profile
+      # see limited student profiles
+      # read, update appointments (only statuses)
+      # manage Availabilities
+      # read, update Feedback
       can :access, :rails_admin
       can :dashboard
+      can :read, User, :id => user.id
       can :manage, Availability, :instructor_id => user.id
+      can :read, Appointment, :instructor_id => user.id
+      can :update, Appointment, :instructor_id => user.id
     else
       can :manage, User, :id => user.id
     end

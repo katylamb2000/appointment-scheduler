e38f680196fd02c4634b5bf69cc5a5fef7cb5ab0
Tam T Dang <teedang19@gmail.com>
2014-11-22 10:20:43 -0600

    todays_availability model and rails admin config, same for availability

---
+++ app/models/todays_availability.rb
@@ -0,0 +1,48 @@
+class TodaysAvailability < Availability
+  default_scope { where('start_time > ?', Date.today.beginning_of_day).where('end_time < ?', Date.today.end_of_day) }
+
+  rails_admin do
+    label do
+      "Today's Availability"
+    end
+
+    list do
+      field :id
+      field :instructor
+      field :start_time
+      field :end_time
+    end
+
+    show do
+      field :id
+      field :instructor
+      field :start_time
+      field :end_time
+      field :created_at do
+        visible do
+          bindings[:view].current_user.admin?
+        end
+      end
+      field :updated_at do
+        visible do
+          bindings[:view].current_user.admin?
+        end
+      end
+    end
+
+    edit do
+      field :instructor do
+        inline_add false
+        inline_edit false
+        visible do
+          bindings[:view].current_user.admin?
+        end
+        associated_collection_scope do
+          Proc.new { |scope| scope = scope.where(instructor: true) }
+        end
+      end
+      field :start_time
+      field :end_time
+    end
+  end
+end

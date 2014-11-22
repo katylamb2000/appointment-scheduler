52c78087d2d33f218bf83505d286a18c50f3a377
Tam T Dang <teedang19@gmail.com>
2014-11-22 09:21:54 -0600

    rails admin config for todays_appointment, same as appointment

--- app/models/todays_appointment.rb
+++ app/models/todays_appointment.rb
@@ -5,5 +5,107 @@ class TodaysAppointment < Appointment
     label do
       "Today's Appointment"
     end
+
+    list do
+      field :id
+      field :instructor
+      field :user do
+        label do
+          "Student"
+        end
+      end
+      field :appointment_category
+      field :start_time
+      field :status
+    end
+
+    show do
+      field :id
+      field :instructor
+      field :user do
+        label do
+          "Student"
+        end
+      end
+      field :appointment_category
+      field :start_time
+      field :end_time
+      field :status
+      field :created_at do
+        visible do
+          bindings[:view].current_user.admin?
+        end
+      end
+      field :updated_at do
+        visible do
+          bindings[:view].current_user.admin?
+        end
+      end
+    end
+
+    edit do
+      field :instructor do
+        inline_add false
+        inline_edit false
+
+        visible do
+          bindings[:view].current_user.admin?
+        end
+
+        associated_collection_scope do
+          Proc.new { |scope| scope = scope.where(instructor: true) }
+        end
+      end
+
+      field :user do
+        inline_add false
+        inline_edit false
+
+        label do
+          "Student"
+        end
+
+        read_only do
+          bindings[:view].current_user.instructor?
+        end
+
+        help do
+          bindings[:view].current_user.instructor? ? "" : "#{help}"
+        end
+
+        associated_collection_scope do
+          Proc.new { |scope| scope = scope.where(instructor: false).where(admin: false) }
+        end
+      end
+
+      field :appointment_category do
+        inline_add false
+        inline_edit false
+
+        read_only do
+          bindings[:view].current_user.instructor?
+        end
+
+        help do
+          bindings[:view].current_user.instructor? ? "" : "#{help}"
+        end
+      end
+
+      field :start_time do
+        read_only do
+          bindings[:view].current_user.instructor?
+        end
+
+        help do
+          bindings[:view].current_user.instructor? ? "" : "#{help}"
+        end
+      end
+      
+      field :status, :enum do
+        enum do
+          ["Open", "Future", "Past - Occurred", "Cancelled by Student", "Cancelled by Instructor", "Rescheduled by Student", "Rescheduled by Instructor", "No Show"]
+        end
+      end
+    end
   end
 end

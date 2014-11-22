44064415849a0889065e882a4267f9a1a4034f76
Tam T Dang <teedang19@gmail.com>
2014-11-21 21:37:05 -0600

    appointment now sets end time in callback with validation on presence of end time. blank method for overlap validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,21 @@
 class Appointment < ActiveRecord::Base
-  validates_presence_of :instructor_id, :appointment_category_id, :start_time, :status
-  validates :status, inclusion: { in: ["Open", "Future", "Past - Occurred", "Cancelled by Student", "Cancelled by Instructor", "Rescheduled by Student", "Rescheduled by Instructor", "No Show"] }
+  before_validation :set_end_time
 
+  validates_presence_of :instructor_id, :appointment_category_id, :start_time, :end_time, :status
+  validates :status, inclusion: { in: ["Open", "Future", "Past - Occurred", "Cancelled by Student", "Cancelled by Instructor", "Rescheduled by Student", "Rescheduled by Instructor", "No Show"] }
   validates_presence_of :user_id, unless: Proc.new{ |record| record.open? }
+  validate :does_not_overlap_other_appointments
 
   belongs_to :appointment_category
   belongs_to :user
   belongs_to :instructor, class_name: "User"
+
+  def set_end_time
+    self.end_time = (self.start_time + self.appointment_category.total_duration.minutes)
+  end
+
+  def does_not_overlap_other_appointments
+  end
 
   def lesson_duration
     appointment_category.lesson_minutes

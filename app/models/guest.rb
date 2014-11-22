74f4666086197c5f499ce5a051025a9550cb884f
Tam T Dang <teedang19@gmail.com>
2014-11-22 00:37:21 -0600

    rails admin base config for guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -6,11 +6,54 @@ class Guest < User
       bindings[:controller].current_user.admin?
     end
 
+    object_label_method do
+      :full_name
+    end
+
     list do
+      field :id
       field :email
+      field :first_name
+      field :location
+      field :age
+      field :gender
+    end
+
+    show do
+      field :id
+      field :instructor
+      field :admin
+      field :guest
+      field :student, :boolean
+      field :email
+      field :full_name
+      field :gender
+      field :age
+      field :skill_level
+      field :musical_genre
+      field :years_playing
+      field :city
+      field :state
+      field :zip
+      field :country
+      field :sign_in_count
+      field :last_sign_in_at
+      field :created_at
+      field :updated_at
+    end
+
+    edit do
+      field :instructor
+      field :admin
+      field :email
+      field :first_name
+      field :age, :enum do
+        enum do
+          (18...85).to_a
+        end
+      end
       field :city
       field :country
-      field :age
     end
   end
 end

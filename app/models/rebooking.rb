b7152ce00b6811b4bc96526e7f721e0bdc40f253
Tam T Dang <teedang19@gmail.com>
2014-11-22 14:35:11 -0600

    rebooking validations uniqueness on dead appointment and new appointment, as well as presence of both

--- app/models/rebooking.rb
+++ app/models/rebooking.rb
@@ -1,4 +1,8 @@
 class Rebooking < ActiveRecord::Base
+  validates_uniqueness_of :dead_appointment_id
+  validates_uniqueness_of :new_appointment_id
+  validates_presence_of :dead_appointment_id, :new_appointment_id
+
   belongs_to :dead_appointment, class_name: "Appointment"
   belongs_to :new_appointment, class_name: "Appointment"
 end

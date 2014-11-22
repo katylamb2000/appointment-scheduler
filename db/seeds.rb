b35f05a85c3231db4db317b38a56ed931184be40
Tam T Dang <teedang19@gmail.com>
2014-11-21 23:05:13 -0600

    update seedfile for Appointment Categories to reflect new price now that it's not in cents

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ shawn = User.find_by(email: "shawn@sbguitar.com")
 User.create(email: "shawn@sbguitar.com", password: "password", instructor: true, first_name: "Shawn", last_name: "Baldissero") unless shawn
 
 thirty_minute_appt = AppointmentCategory.find_by_lesson_minutes(30)
-AppointmentCategory.create(lesson_minutes: 30, buffer_minutes: 15, price_in_cents: 5000) unless thirty_minute_appt
+AppointmentCategory.create(lesson_minutes: 30, buffer_minutes: 15, price: 50.00) unless thirty_minute_appt
 
 one_hour_appt = AppointmentCategory.find_by_lesson_minutes(60)
-AppointmentCategory.create(lesson_minutes: 60, buffer_minutes: 15, price_in_cents: 9000) unless one_hour_appt
+AppointmentCategory.create(lesson_minutes: 60, buffer_minutes: 15, price: 90.00) unless one_hour_appt

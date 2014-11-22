f4e73fb9d6bc175244748c2c578a2ffbec11c2e0
Tam T Dang <teedang19@gmail.com>
2014-11-21 23:45:45 -0600

    user model has new validations on other fields, enumerations in rails_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, unless: Proc.new { |u| u.admin? }
   validates_presence_of :city, :country, :age, unless: Proc.new { |u| u.admin? || u.instructor? }
   validates :gender, inclusion: { in: ["male", "female"] }, allow_nil: true
+  validates :age, numericality: { greater_than_or_equal_to: 18 }, allow_nil: true
+  validates :skill_level, inclusion: { in: ["Beginner", "Intermediate", "Advanced", "Master"] }, allow_nil: true
+  validates :musical_genre, inclusion: { in: ["Pop", "Jazz", "Classical", "Progressive", "Metal", "Rock", "Country", "Fusion", "Funk", "Other"] }, allow_nil: true
+  validates :years_playing, inclusion: { in: ["1 - 2", "3 - 5", "5 - 10", "10 +"] }, allow_nil: true
 
   # TODO: dependent destroy? or acts as paranoid?
   has_many :availabilities, foreign_key: "instructor_id"
@@ -86,10 +90,26 @@ class User < ActiveRecord::Base
           ["male", "female"]
         end
       end
-      field :age
-      field :skill_level
-      field :musical_genre
-      field :years_playing
+      field :age, :enum do
+        enum do
+          (18...85).to_a
+        end
+      end
+      field :skill_level, :enum do
+        enum do
+          ["Beginner", "Intermediate", "Advanced", "Master"] 
+        end
+      end
+      field :musical_genre, :enum do
+        enum do
+          ["Pop", "Jazz", "Classical", "Progressive", "Metal", "Rock", "Country", "Fusion", "Funk", "Other"]
+        end
+      end
+      field :years_playing, :enum do
+        enum do
+          ["1 - 2", "3 - 5", "5 - 10", "10 +"]
+        end
+      end
       field :city
       field :state
       field :zip

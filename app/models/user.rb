37ca26a38493ecdb2a7983fe02ef3b8fa71e6e94
Tam T Dang <teedang19@gmail.com>
2014-11-26 14:21:31 -0500

    TODO comment about verifying compatibility across languages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  # TODO verify compatibility across languages, specifically non-Arabic alphabets
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :async
 

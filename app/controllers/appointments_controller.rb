a79059567582b4d80b5f74473dbfcc15af4d3dd7
Tam T Dang <teedang19@gmail.com>
2014-11-26 15:06:22 -0500

    Appointments controller index shows only upcoming

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
 
   def index
-    @appointments = Appointment.open_or_booked
+    @appointments = Appointment.upcoming.open_or_booked
     # TODO group by dates
   end
 end

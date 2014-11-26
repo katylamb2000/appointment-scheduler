af129832b3efe3b92236e5b1934c3213e7e37f86
Tam T Dang <teedang19@gmail.com>
2014-11-25 21:36:24 -0500

    basic appointments controller #index which feeds all appointments ordered by start time

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,7 @@
 class AppointmentsController < ApplicationController
+
+  def index
+    # TODO only show taken ("Future") and open appointments - don't show cancelled/rescheduled/unavailable
+    @appointments = Appointment.order(:start_time)
+  end
 end

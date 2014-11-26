bddf8edd682620470267fb3882ec644fe3046a73
Tam T Dang <teedang19@gmail.com>
2014-11-25 19:46:33 -0500

    add rails_admin assets to precompile list in assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Prevent initializing the application before assets are precompiled (required for heroku)
+# config.assets.initialize_on_precompile = false
+# Add Rails Admin assets (required)
+Rails.application.config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']

2324f9cf77f62e66365ebd30c71352f57d7adff0
Tam T Dang <teedang19@gmail.com>
2014-12-14 12:24:26 -0600

    long TODO comment about reserving and potential pseudocode process

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -16,6 +16,13 @@ class AppointmentsController < ApplicationController
         redirect_to appointments_path, alert: "Rat Shardz!"
       end
     else
+      # TODO reserve for 15 minutes?
+        # change status to Reserved (On Hold), Trying to be Booked, etc
+        # set sidekiq process in 15 minutes, send appointment id
+        # when sidekiq process runs, check to see if that appointment has been paid for
+        # if not, set appointment to Open
+        # but how do we locate that session for that user to kick them out of the booking process?
+        # can we implement a countdown timer?
       render 'users/authenticate'
     end
   end

6dab7930909c2ba9452c885cf15762a4f41f6a00
Tam T Dang <teedang19@gmail.com>
2014-12-14 19:30:43 -0600

    appointments controller -- if current user, render confirm modal

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -18,14 +18,10 @@ class AppointmentsController < ApplicationController
     end
 
     if current_user
-      @appointment.user = current_user
-      @appointment.status = "Booked - Future"
-      if @appointment.save
-        redirect_to appointments_path, notice: "Huzzah!"
-      else
-        redirect_to appointments_path, alert: "Rat Shardz!"
-      end
+      render action: "confirm_modal"
     else
+      render "users/auth" # modal (JS)
+      # render "users/authenticate" # haml view (HTML)
       # TODO reserve for 15 minutes?
         # change status to Reserved (On Hold), Trying to be Booked, etc
         # set sidekiq process in 15 minutes, send appointment id
@@ -33,8 +29,6 @@ class AppointmentsController < ApplicationController
         # if not, set appointment to Open
         # but how do we locate that session for that user to kick them out of the booking process?
         # can we implement a countdown timer?
-      render "users/auth" # modal (JS)
-      # render "users/authenticate" # haml view (HTML)
     end
   end
 

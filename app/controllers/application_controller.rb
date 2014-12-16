5fd52f4833fb9561c07acff21487db919556f60f
Tam T Dang <teedang19@gmail.com>
2014-12-15 22:19:27 -0600

    around filter to set time zone in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_filter :set_timezone
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = exception.message
@@ -11,9 +12,16 @@ class ApplicationController < ActionController::Base
     (resource.admin? || resource.instructor?) ? rails_admin_path : root_path
   end
 
+  private
+
+    def set_timezone
+      client_timezone = cookies[:timezone]
+      Time.use_zone(client_timezone) { yield }
+    end
+
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :city, :country, :age]
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:first_name, :city, :country, :age]
+    end
 end

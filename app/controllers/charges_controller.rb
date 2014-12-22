db9e6a8007eca92dabfff3a224ae6a863be19b97
Tam T Dang <teedang19@gmail.com>
2014-12-21 22:32:43 -0600

    small updates to charges_controller#create using what i learned from VeryMoved

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,15 +1,18 @@
 class ChargesController < ApplicationController
 
   def create
-    @amount = params[:appointment][:price].to_i
-    @appointment = Appointment.find(params[:appointment][:id])
-    customer = current_user.stripe_customer
+    amount = params[:appointment][:price].to_i
+    appointment = Appointment.find(params[:appointment][:id])
+    stripe_customer = current_user.stripe_customer
+    stripe_customer.cards.create(card: params[:stripeToken]) # TODO what if they already have a saved 
 
     charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
+      :customer    => stripe_customer.id,
+      :amount      => amount,
       :description => 'Testing123',
       :currency    => 'usd',
+      :statement_descriptor => 'sbguitar.com appt'
+      :receipt_email => current_user.email,
       :metadata   => {
         rails_appt_id: params[:appointment][:id],
         rails_customer_id: current_user.id
@@ -24,7 +27,7 @@ class ChargesController < ApplicationController
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
-      redirect_to charges_path
+      redirect_to charges_path and return
   end
 
 end

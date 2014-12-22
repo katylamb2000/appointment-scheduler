4b9fa12fe0143f35ec9d914c69044df90c3a8219
Tam T Dang <teedang19@gmail.com>
2014-12-21 23:35:31 -0600

    spatial changes to ability.rb with exception of allowing all other users to read Appointments where the user id is them

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,28 +7,26 @@ class Ability
       can :access, :rails_admin
       can :dashboard
       can :manage, :all
-    elsif user.instructor? # TODO read, update Feedback
+    elsif user.instructor?
+      # TODO read, update Feedback
       can :access, :rails_admin
       can :dashboard
-
-      # update own profile
-      can :read, User, :id => user.id
-      can :update, User, :id => user.id
-      can :export, User, :id => user.id
-
-      # see limited student profiles
-      can :read, User, appointments: { instructor_id: user.id }
-      can :export, User, appointments: { instructor_id: user.id }
-
-      # manage Availabilities
-      can :manage, Availability, :instructor_id => user.id
-      can :export, Availability, :instructor_id => user.id
-
       # read, update appointments
       can :read, Appointment, :instructor_id => user.id
       can :update, Appointment, :instructor_id => user.id
       can :export, Appointment, :instructor_id => user.id
+      # manage Availabilities
+      can :manage, Availability, :instructor_id => user.id
+      can :export, Availability, :instructor_id => user.id
+      # update own profile
+      can :read, User, :id => user.id
+      can :update, User, :id => user.id
+      can :export, User, :id => user.id
+      # see limited student profiles
+      can :read, User, appointments: { instructor_id: user.id }
+      can :export, User, appointments: { instructor_id: user.id }
     else
+      can :read, Appointment, :user_id => user.id
       can :manage, User, :id => user.id
     end
   end

cb05f00f038540d02b4c2b8835835d7740750708
Tam T Dang <teedang19@gmail.com>
2014-12-23 12:39:57 -0600

    add route for /dashboard path

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
 
   resources :availabilities # TODO remove? currently implemented via rails_admin
   resources :appointments, only: [:index, :show, :update]
+  get 'dashboard' => 'appointments#dashboard', as: 'student_dashboard'
   post 'charges' => "charges#create"
 end

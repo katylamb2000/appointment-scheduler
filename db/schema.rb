de9b97225db6e134c6d8d79ca862e8536ab49299
Tam T Dang <teedang19@gmail.com>
2014-12-24 08:59:19 -0600

    migration to add profile_photo string column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223004118) do
+ActiveRecord::Schema.define(version: 20141224145828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20141223004118) do
     t.boolean  "guest",                  default: false
     t.string   "stripe_id"
     t.boolean  "accepts_age_agreement",  default: false
+    t.string   "profile_photo"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

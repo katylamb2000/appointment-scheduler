43506b3946c355a4f8311e4badbf116bcb9defbb
Tam T Dang <teedang19@gmail.com>
2014-12-25 16:56:45 -0500

    add attachment_processing bool flag to lesson_materials table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225214913) do
+ActiveRecord::Schema.define(version: 20141225215548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20141225214913) do
     t.string   "attachment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "attachment_processing", default: false, null: false
   end
 
   create_table "rebookings", force: true do |t|

c807f7105238f21e7dbed440665a4d264ab0bcf7
Tam T Dang <teedang19@gmail.com>
2014-12-25 16:50:51 -0500

    lesson_material change presence validation of document to attachment

--- app/models/lesson_material.rb
+++ app/models/lesson_material.rb
@@ -1,5 +1,5 @@
 class LessonMaterial < ActiveRecord::Base
-  validates_presence_of :instructor_id, :name, :document
+  validates_presence_of :instructor_id, :name, :attachment
   validates_uniqueness_of :name
 
   belongs_to :instructor, class_name: "User"

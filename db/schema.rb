b6855c8342ddebbbe537843daf8dd05c78b19b44
Tam T Dang <teedang19@gmail.com>
2014-12-25 16:50:19 -0500

    migration to rename document to attachment on lesson_materials table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225213621) do
+ActiveRecord::Schema.define(version: 20141225214913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20141225213621) do
     t.integer  "instructor_id"
     t.string   "name"
     t.text     "description"
-    t.string   "document"
+    t.string   "attachment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

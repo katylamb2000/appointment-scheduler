363396ea4eaa13481c826d4bcaa7f1b1110ac75c
Tam T Dang <teedang19@gmail.com>
2014-12-25 19:53:26 -0500

    rails_admin base config for student_material

--- app/models/student_material.rb
+++ app/models/student_material.rb
@@ -3,4 +3,50 @@ class StudentMaterial < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :lesson_material
+
+  rails_admin do
+    list do
+      field :id
+      field :user
+      field :lesson_material
+      field :notes
+    end
+
+    show do
+      field :id
+      field :user
+      field :lesson_material
+      field :notes
+    end
+
+    create do
+      field :user do
+        inline_add false
+        inline_edit false
+      end
+      field :lesson_material do
+        inline_add false
+        inline_edit false
+      end
+      field :notes
+    end
+
+    edit do
+      field :user do
+        read_only do
+          !(bindings[:view].current_user.admin?)
+        end
+        inline_add false
+        inline_edit false
+      end
+      field :lesson_material do
+        read_only do
+          !(bindings[:view].current_user.admin?)
+        end
+        inline_add false
+        inline_edit false
+      end
+      field :notes
+    end
+  end
 end

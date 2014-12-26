74a6f690b1bd1839525d03021f181b3b02cd3b75
Tam T Dang <teedang19@gmail.com>
2014-12-25 19:00:29 -0500

    rails_admin base config for lesson_material

--- app/models/lesson_material.rb
+++ app/models/lesson_material.rb
@@ -6,4 +6,44 @@ class LessonMaterial < ActiveRecord::Base
 
   mount_uploader :attachment, AttachmentUploader
   process_in_background :attachment
+
+  rails_admin do
+    list do
+      field :id
+      field :instructor do
+        visible do
+          bindings[:controller].current_user.admin?
+        end
+      end
+      field :name
+      field :attachment
+    end
+
+    show do
+      field :id
+      field :instructor do
+        visible do
+          bindings[:controller].current_user.admin?
+        end
+      end
+      field :name
+      field :description
+      field :attachment
+    end
+
+    edit do
+      field :instructor do
+        inline_add false
+        inline_edit false
+        visible do
+          bindings[:controller].current_user.admin?
+        end
+      end
+      field :name
+      field :description
+      field :attachment
+    end
+    edit do
+    end
+  end
 end

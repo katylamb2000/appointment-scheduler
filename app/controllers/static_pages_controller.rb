47948302cdcd10e06c0788d7eb84aa7929b61fed
Tam T Dang <teedang19@gmail.com>
2014-12-27 13:40:15 -0500

    refactor comment about IG feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,7 @@ class StaticPagesController < ApplicationController
     client = Instagram.client
     shawns_id = "1300104176"
     @shawns_feed = client.user_recent_media(shawns_id) # can pass count: n ; default returns 20
+    # TODO refactor
   end
 
   def faq

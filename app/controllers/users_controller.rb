e59b2883a82402da46b373a8f2e826dc0a8e9e3f
Tam T Dang <teedang19@gmail.com>
2014-12-27 12:16:20 -0500

    users_controller #student_dashboard sends down current_users learning materials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def student_dashboard
     @past_appts = current_user.past_appointments
     @upcoming_appts = current_user.upcoming_appointments
+    @materials = current_user.learning_materials
   end
-  
+
 end

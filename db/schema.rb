90160eec9bafda5eeff9473135af82f2283873a0
Tam T Dang <teedang19@gmail.com>
2015-01-01 14:23:06 -0500

    migration to add confirmable devise fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231164204) do
+ActiveRecord::Schema.define(version: 20150101192058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,8 +105,12 @@ ActiveRecord::Schema.define(version: 20141231164204) do
     t.string   "profile_photo"
     t.boolean  "profile_photo_processing", default: false, null: false
     t.datetime "deleted_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree

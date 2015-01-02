ea73d3a839428dcb0eddb8e30d168bc4eb3ee3ee
Tam T Dang <teedang19@gmail.com>
2015-01-02 15:26:57 -0500

    basic validations on feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,5 @@
 class Feedback < ActiveRecord::Base
+  validates_presence_of :user_id, :appointment_id, :notes, :context
+  validates :context, inclusion: { in: ["left by instructor", "left by student"] }
+  validates_uniqueness_of :user_id, scope: :appointment_id
 end

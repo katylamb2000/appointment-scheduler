7effc9a2f790d6a1ea7c420ea56475a4159940a5
Tam T Dang <teedang19@gmail.com>
2015-01-03 15:32:26 -0500

    update to seedfile now that using STI

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
-admin = User.find_by(admin: true)
-User.create(email: "admin@sbguitar.com", password: "password", admin: true, first_name: "Tam", last_name: "Dang") unless admin
+Admin.create(email: "admin@sbguitar.com", password: "password", admin: true, first_name: "Tam", last_name: "Dang")
 
-shawn = User.find_by(email: "shawn@sbguitar.com")
-User.create(email: "shawn@sbguitar.com", password: "password", instructor: true, first_name: "Shawn", last_name: "Baldissero") unless shawn
+Instructor.create(email: "shawn@sbguitar.com", password: "password", first_name: "Shawn", last_name: "Baldissero")
 
-todd = User.find_by(email: "todd@sbguitar.com")
-User.create(email: "todd@sbguitar.com", password: "password", instructor: true, first_name: "Todd", last_name: "Dang-Baldissero") unless todd
+Instructor.create(email: "todd@sbguitar.com", password: "password", first_name: "Todd", last_name: "Dang-Baldissero")
 
-goop = User.find_by(email: "goop@cats.com")
-User.create(email: "goop@cats.com", password: "password", first_name: "Frau Goop", last_name: "The Ruthless Cat", city: "Chicago", country: "US", age: 20) unless goop
+Student.create(email: "goop@cats.com", password: "password", first_name: "Frau Goop", last_name: "The Ruthless Cat", city: "Chicago", country: "US", age: 20, accepts_age_agreement: true, confirmed_at: Time.now)
 
 thirty_minute_appt = AppointmentCategory.find_by_lesson_minutes(30)
 AppointmentCategory.create(lesson_minutes: 30, buffer_minutes: 15, price: 50.00) unless thirty_minute_appt

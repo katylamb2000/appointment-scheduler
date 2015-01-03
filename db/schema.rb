18bfb23bc314410ab511d7009d2a086f1005ca7a
Tam T Dang <teedang19@gmail.com>
2015-01-03 13:56:14 -0500

    migration to remove instructor boolean from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150103183827) do
+ActiveRecord::Schema.define(version: 20150103185527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 20150103183827) do
     t.string   "musical_genre"
     t.string   "years_playing"
     t.boolean  "admin",                    default: false
-    t.boolean  "instructor",               default: false
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "stripe_id"

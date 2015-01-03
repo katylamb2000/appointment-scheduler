fdc680ae8ab806ecae37b283a391956c1f1d6a67
Tam T Dang <teedang19@gmail.com>
2015-01-03 13:37:39 -0500

    remove override of devise #password_required? since we no longer have guest users

--- app/models/user.rb
+++ app/models/user.rb
@@ -358,10 +358,6 @@ class User < ActiveRecord::Base
 
   protected
 
-    def password_required? # overrride Devise method so that Guest Users do not need passwords
-      guest? ? false : (!persisted? || !password.nil? || !password_confirmation.nil?)
-    end
-
     def confirmation_required?
       student?
     end

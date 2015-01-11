6766dd73a8362cea28835b2e8758efd091086dae
Tam T Dang <teedang19@gmail.com>
2015-01-11 16:29:11 -0600

    Availability#description model

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -36,6 +36,10 @@ class Availability < ActiveRecord::Base
     "##{id}"
   end
 
+  def description
+    start_time.strftime("%a %m/%e, %l:%M %p") + " - " + end_time.strftime("%a %m/%e, %l:%M %p")
+  end
+
   def can_be_edited?
     errors.add(:base, "This availability has upcoming appointments. Please cancel or reschedule them in order to edit this availability.") unless !(has_pending_appointments?)
   end

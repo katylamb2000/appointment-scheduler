992041ab8b5c399dce4ed8c10ba4e8c0b8fd72f0
Tam T Dang <teedang19@gmail.com>
2015-01-11 16:28:57 -0600

    availabilities crud methods in controller

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,6 +1,29 @@
 class AvailabilitiesController < ApplicationController # TODO remove? currently implemented via rails_admin
 
   def index
-    @availabilities = current_user.availabilities
+    if current_user.admin?
+      @availabilities = Availability.all
+    else
+      @availabilities = current_user.availabilities
+    end
+  end
+
+  def new
+    @availability = Availability.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end

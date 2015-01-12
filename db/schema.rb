ae5a9bb74417961318a1ebbc74ee52ce359d3082
Tam T Dang <teedang19@gmail.com>
2015-01-11 20:07:46 -0600

    migration to add number_of_occurrences and schedule_end_date to availabilities table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104231236) do
+ActiveRecord::Schema.define(version: 20150112020634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20150104231236) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "schedule"
+    t.integer  "number_of_occurrences"
+    t.datetime "schedule_end_date"
   end
 
   create_table "feedbacks", force: true do |t|

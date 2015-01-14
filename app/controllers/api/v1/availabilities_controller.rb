e2ee0efacfe4b7f645075e4bcd2fc7b1bd998e93
Tam T Dang <teedang19@gmail.com>
2015-01-14 17:47:41 -0600

    availabilities_controller (api) index returns json of availabilities after locating the user by id

--- app/controllers/api/v1/availabilities_controller.rb
+++ app/controllers/api/v1/availabilities_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::AvailabilitiesController < Api::V1::ApiController
   def index
+    user = User.find(params[:user_id])
+    @availabilities = user.availabilities
+    render json: @availabilities, status: :ok and return
   end
 end

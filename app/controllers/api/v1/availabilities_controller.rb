d0db4ace694142cc4edf961e2488873558e8508a
Tam T Dang <teedang19@gmail.com>
2015-01-14 19:07:38 -0600

    availabilities (api) #index ensures the user is an instructor

--- app/controllers/api/v1/availabilities_controller.rb
+++ app/controllers/api/v1/availabilities_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::AvailabilitiesController < Api::V1::ApiController
   def index
-    user = User.find(params[:user_id])
-    @availabilities = user.availabilities
+    @user = User.find(params[:user_id])
+    return user_is_not_instructor! unless @user.instructor?
+
+    @availabilities = @user.availabilities
     render json: @availabilities, status: :ok and return
   end
 
@@ -9,5 +11,9 @@ class Api::V1::AvailabilitiesController < Api::V1::ApiController
 
     def default_serializer_options
       { root: false }
+    end
+
+    def user_is_not_instructor!
+      render json: { errors: [ "User is not an instructor" ] }, status: :unprocessable_entity and return
     end
 end

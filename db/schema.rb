75c119dbc3d1c1e63dc8bf1ce8d1fd07ef17e10b
Tam T Dang <teedang19@gmail.com>
2015-01-14 21:46:37 -0600

    migration to index feedbacks table by appointment id and user id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115034439) do
+ActiveRecord::Schema.define(version: 20150115034530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(version: 20150115034439) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "feedbacks", ["appointment_id"], name: "index_feedbacks_on_appointment_id", using: :btree
+  add_index "feedbacks", ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
 
   create_table "lesson_materials", force: true do |t|
     t.integer  "instructor_id"

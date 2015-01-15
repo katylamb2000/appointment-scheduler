5daa1f163c8890e7838eb0433941d8e25cb0c774
Tam T Dang <teedang19@gmail.com>
2015-01-14 20:23:48 -0600

    remove to in availability #title (serializer)

--- app/serializers/availability_serializer.rb
+++ app/serializers/availability_serializer.rb
@@ -2,7 +2,7 @@ class AvailabilitySerializer < ActiveModel::Serializer
   attributes :id, :title, :start, :end
 
   def title # TODO is this sloppy?
-    "to #{object.end_time.strftime('%l:%M %p')}"
+    " - #{object.end_time.strftime('%l:%M %p')}"
   end
 
   def start

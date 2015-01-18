c3d176e59bad733c6bf4b16465f6f8c5bb263ab8
Tam T Dang <teedang19@gmail.com>
2015-01-18 11:25:24 -0600

    update to spacing in avail_calendar.js, too many tabs before

--- app/assets/javascripts/avail_calendar.js
+++ app/assets/javascripts/avail_calendar.js
@@ -1,25 +1,25 @@
 $(document).on('page:update', function(){
 
-    $('#avail-calendar').fullCalendar({
-      events: {
-        url: 'api/v1/availabilities.json',
-        type: 'GET',
-        data: {
-          user_id: $('#avail-calendar').attr('data-user'),
-        }
-      },
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      dayClick: function(date, jsEvent, view) {
-        $.ajax({
-          url: '/availabilities/new.js',
-          type: 'GET',
-          data: { start_date: date.format() }
-        });
+  $('#avail-calendar').fullCalendar({
+    events: {
+      url: 'api/v1/availabilities.json',
+      type: 'GET',
+      data: {
+        user_id: $('#avail-calendar').attr('data-user'),
       }
-    })
+    },
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    dayClick: function(date, jsEvent, view) {
+      $.ajax({
+        url: '/availabilities/new.js',
+        type: 'GET',
+        data: { start_date: date.format() }
+      });
+    }
+  })
 
 });
